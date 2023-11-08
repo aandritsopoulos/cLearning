dad97b1dbe86c3fd6bfd93f1953571789e41af6a
Alexandros Andritsopoulos <109956051+aandritsopoulos@users.noreply.github.com>
2023-11-08 17:49:45 +0200

    Add files via upload

--- lab3.2.py
+++ lab3.2.py
@@ -1,4 +1,4 @@
-import numpy as np
+"""import numpy as np
 import matplotlib.pyplot as plt
 
 n = np.arange ( -10 , 10 )
@@ -8,7 +8,21 @@ x3 = -4* (-n+2==0).astype(int)
 x4 = abs(2 * (n-3>=0) - 3* x2 - (n - 2) * (n-2>=0) -x3)
 
 x5 = np.sin(n) /n
-x5[n == 0 ] = 1 
+x5[n == 0 ] = 1 """
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+n = np.arange( -10 , 10 )
+x1 = np.exp(np.abs(n))
+e1 = np.sum(np.abs(x1)**2)
+print(f'Energy of x1: {e1}')
+
+
+ 
+
+
+
 
 
 
